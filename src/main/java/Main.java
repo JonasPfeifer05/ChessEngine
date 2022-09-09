b2628021aeed120d11784604b3dee0316fee198e
Jonas Pfeifer <jonaspfeifer@drei.at>
2022-09-09 15:11:32 +0200

    Revert "fixed kings movement"
    This reverts commit ee83ed42b73a226b650d078769df6c853ca5b96d.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,8 +5,6 @@ import util.Asset;
 import util.Player;
 import util.Position;
 
-import java.awt.*;
-
 public class Main {
     public static void main(String[] args) {
         System.out.println("Huff ist dumm!");
@@ -15,6 +13,12 @@ public class Main {
         System.out.println("Hello World");
 
         Engine engine = new Engine(4);
+        engine.board.set(new Position(2, 3), new Rook(Player.PLAYER1));
+        engine.board.set(new Position(3, 2), new Rook(Player.PLAYER1));
+        engine.board.set(new Position(10, 11), new Rook(Player.PLAYER1));
+        engine.board.set(new Position(11, 10), new Rook(Player.PLAYER1));
+
+        engine.getAllValidMoves(new Position(10, 12));
 
         Asset.setUp();
         new Window(480 / 9 * 16, 480, engine);
